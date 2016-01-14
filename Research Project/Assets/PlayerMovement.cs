3046f3bb7b68dcd2cc57d27c22f8ed73dbd49608
Greg Weil <gregweil@pentavera.com>
2016-01-14 15:00:27 -0500

    Basic movement and jumping

---
+++ Research Project/Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 7.5f;
+    public float acceleration = 100.0f;
+
+    public float jumpVelocity = 15.0f;
+    public float gravity = -50.0f;
+
+    CharacterController controller = null;
+    Vector3 movement = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement.y += gravity * Time.deltaTime;
+        if (controller.isGrounded && Input.GetButton("Jump")) {
+            movement.y += jumpVelocity;
+        }
+
+        Vector2 goalMovement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 currentMovement = Vector2.MoveTowards(new Vector2(movement.x, movement.z),
+            (goalMovement * moveSpeed), (acceleration * Time.deltaTime));
+        movement = new Vector3(currentMovement.x, movement.y, currentMovement.y);
+
+        controller.Move(movement * Time.deltaTime);
+        if (controller.isGrounded) movement.y = 0.0f;
+	}
+}
