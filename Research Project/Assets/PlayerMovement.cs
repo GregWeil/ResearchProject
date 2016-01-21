531e54de4e08974465f842740c41a1308247ed1f
Greg Weil <gregweil@pentavera.com>
2016-01-21 00:09:18 -0500

    Added comments

--- Research Project/Assets/PlayerMovement.cs
+++ Research Project/Assets/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour {
 
     CharacterController controller = null;
     Vector3 movement = Vector3.zero;
+    float rotVelocity = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -21,19 +22,22 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //Apply gravity
         movement.y += gravity * Time.deltaTime;
-        if (controller.isGrounded && Input.GetButton("Jump")) {
-            movement.y += jumpVelocity;
-        }
+        if (controller.isGrounded) movement.y = 0.0f;
 
+        //Movement from input
         Vector3 moveBase = cameraTransform.TransformDirection(Vector3.right);
         moveBase.y = 0;
         moveBase.Normalize();
         Vector3 goalMovement = (Input.GetAxis("Horizontal") * moveBase) + (Input.GetAxis("Vertical") * new Vector3(-moveBase.z, 0, moveBase.x));
         Vector3 currentMovement = Vector3.MoveTowards(movement, (goalMovement * moveSpeed), (acceleration * Time.deltaTime));
         movement = new Vector3(currentMovement.x, movement.y, currentMovement.z);
+        if (controller.isGrounded && Input.GetButton("Jump")) {
+            movement.y += jumpVelocity;
+        }
 
+        //Apply movement
         controller.Move(movement * Time.deltaTime);
-        if (controller.isGrounded) movement.y = 0.0f;
 	}
 }
