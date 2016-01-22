1647679efeb381f0b1a8d4ca64ff9a9b789d2cab
Greg Weil <gregweil@pentavera.com>
2016-01-21 19:52:57 -0500

    Added a block that damages the player while they are within it

---
+++ Research Project/Assets/DamageField.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageField : MonoBehaviour {
+
+    public float damageRate = 1.0f;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnTriggerStay(Collider other) {
+        other.SendMessage("Damage", (damageRate * Time.fixedDeltaTime), SendMessageOptions.DontRequireReceiver);
+    }
+}
