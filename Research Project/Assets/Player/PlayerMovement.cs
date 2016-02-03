b1eff6e9da84b5919c6bb126e51ecdbaec0a1198
Greg Weil <weilg@rpi.edu>
2016-02-03 01:41:22 -0500

    Reworked player to use a rigidbody

--- Research Project/Assets/Player/PlayerMovement.cs
+++ Research Project/Assets/Player/PlayerMovement.cs
@@ -3,63 +3,71 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float moveSpeed = 7.5f;
-    public float acceleration = 100.0f;
-    public float rotationSpeed = 540.0f;
+    //Desired velocity
+    Vector3 movement = Vector3.zero;
 
-    public float jumpVelocity = 15.0f;
-    public float gravity = -50.0f;
+    //Grounded
+    bool grounded = false;
+    Vector3 groundNormal = Vector3.up;
+    float groundAngle = 0f;
 
     Transform cameraTransform = null;
-
-    CharacterController controller = null;
-    Vector3 movement = Vector3.zero;
+    Rigidbody body = null;
 
 	// Use this for initialization
 	void Start () {
-        controller = GetComponent<CharacterController>();
+        body = GetComponent<Rigidbody>();
         cameraTransform = FindObjectOfType<Camera>().transform;
 	}
 	
+    void FixedUpdate () {
+        body.AddForce(50.0f * -groundNormal);
+
+        //Update velocity
+        Vector3 vel = new Vector3(body.velocity.x, 0.0f, body.velocity.z);
+        body.AddForce(15.0f * ((5.0f * movement) - vel));
+
+        //Rotate to face forward
+        if (movement.sqrMagnitude > 0.1f) {
+            body.MoveRotation(Quaternion.RotateTowards(body.rotation, Quaternion.LookRotation(movement, Vector3.up), (500.0f * Time.fixedDeltaTime)));
+        }
+
+        //Reset grounded
+        grounded = false;
+        if (!grounded) {
+            groundNormal = Vector3.up;
+            groundAngle = 180f;
+        }
+    }
+
+    void OnCollisionStay (Collision col) {
+        foreach (var contact in col.contacts) {
+            var angle = Vector3.Angle(contact.normal, Vector3.up);
+            if (angle < Mathf.Min(45f, groundAngle)) {
+                groundAngle = angle;
+                groundNormal = contact.normal;
+                grounded = true;
+            }
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
-        //Apply gravity
-        movement.y += gravity * Time.deltaTime;
-        if (controller.isGrounded) movement.y = 0.0f;
-
-        //Movement from input
+        ///Update desired movement
         Vector3 moveBase = cameraTransform.TransformDirection(Vector3.right);
         moveBase.y = 0;
         moveBase.Normalize();
-        Vector3 goalMovement = (Input.GetAxis("Horizontal") * moveBase) + (Input.GetAxis("Vertical") * new Vector3(-moveBase.z, 0, moveBase.x));
-        Vector3 currentMovement = Vector3.MoveTowards(movement, (goalMovement * moveSpeed), (acceleration * Time.deltaTime));
-        movement = new Vector3(currentMovement.x, movement.y, currentMovement.z);
-        if (controller.isGrounded && Input.GetButton("Jump")) {
-            movement.y += jumpVelocity;
+        movement = (Input.GetAxis("Horizontal") * moveBase) + (Input.GetAxis("Vertical") * new Vector3(-moveBase.z, 0, moveBase.x));
+        if (movement.sqrMagnitude > 1.0f) movement.Normalize();
+        
+        if (Input.GetButtonDown("Jump") && grounded) {
+            body.velocity += (15.0f * groundNormal);
+            grounded = false;
         }
-
-        //Rotate to face forward
-        if (goalMovement.sqrMagnitude > 0.1f) {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(goalMovement, Vector3.up), (rotationSpeed * Time.deltaTime));
-        }
-
-        //Apply movement
-		Vector3 groundMovement = Vector3.Scale(movement, new Vector3(1, 0, 1));
-		if (controller.isGrounded && movement.y <= 0) {
-			movement.y = Mathf.Min (-groundMovement.magnitude * Mathf.Tan(controller.slopeLimit), movement.y);
-		}
-		controller.Move(movement * Time.deltaTime);
 
         //Die if below the map
         if (transform.position.y < 0.0f) {
             SendMessage("Damage", Time.deltaTime);
         }
 	}
-
-    void OnControllerColliderHit(ControllerColliderHit hit) {
-        Rigidbody body = hit.collider.attachedRigidbody;
-        if (body == null || body.isKinematic) return;
-        
-        body.AddForceAtPosition(-hit.normal * 2000.0f * Time.deltaTime, hit.point);
-    }
 }
