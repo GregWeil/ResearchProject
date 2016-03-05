a1d884ac2afce45fa7b804e73b433db4cd468aba
Greg Weil <weilg@rpi.edu>
2016-03-05 16:22:05 -0500

    Added an empty state machine behavior, the editor now tracks what machine is selected and hides everything if one is not selected

--- Research Project/Assets/Editor/StateMachineEditor.cs
+++ Research Project/Assets/Editor/StateMachineEditor.cs
@@ -4,12 +4,19 @@ using System.Collections;
 
 public class StateMachineEditor : EditorWindow {
 
-    Rect windowRect = new Rect(512, 128, 128, 128);
-
     [MenuItem("Window/State Machine")]
     static void ShowEditor() {
         var editor = GetWindow<StateMachineEditor>();
         editor.Show();
+    }
+
+    StateMachine machine = null;
+
+    Rect windowRect = new Rect(512, 128, 128, 128);
+
+    public void ShowWithTarget(StateMachine target) {
+        machine = target;
+        Show();
     }
 
     void OnEnable() {
@@ -17,14 +24,20 @@ public class StateMachineEditor : EditorWindow {
     }
 
     void OnGUI() {
-        BeginWindows();
-            windowRect = GUILayout.Window(0, windowRect, DrawWindow, "Window");
-        EndWindows();
+        if (machine == null) {
+            GUILayout.Label("No state machine selected!");
+        } else {
+            BeginWindows();
+                windowRect = GUILayout.Window(0, windowRect, DrawWindow, "Window", GUI.skin.box);
+            EndWindows();
 
-        GUILayout.BeginArea(new Rect(0, 0, 250, position.height), GUI.skin.box);
-            GUILayout.Label("Hello");
-            GUILayout.Button("Button");
-        GUILayout.EndArea();
+            GUILayout.BeginArea(new Rect(0, 0, 250, position.height), GUI.skin.box);
+            GUILayout.Label(machine.gameObject.name);
+            if (GUILayout.Button("Select Game Object")) {
+                Selection.activeGameObject = machine.gameObject;
+            }
+            GUILayout.EndArea();
+        }
     }
 
     void DrawWindow(int id) {
---
+++ Research Project/Assets/Editor/StateMachineInspector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(StateMachine))]
+public class StateMachineInspector : Editor {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public override void OnInspectorGUI() {
+        var machine = (StateMachine)target;
+
+        if (GUILayout.Button("Open Editor")) {
+            var editor = EditorWindow.GetWindow<StateMachineEditor>();
+            editor.ShowWithTarget(machine);
+        }
+    }
+}
