3dbf65d40ac817d27fd9b9271380c6951bcee067
Greg Weil <weilg@rpi.edu>
2016-03-05 17:01:06 -0500

    Added a state class, currently just a name and a position. There is a button to add a state, and states can be dragged around. They get saved with the object

--- Research Project/Assets/Editor/StateMachineEditor.cs
+++ Research Project/Assets/Editor/StateMachineEditor.cs
@@ -12,8 +12,6 @@ public class StateMachineEditor : EditorWindow {
 
     StateMachine machine = null;
 
-    Rect windowRect = new Rect(512, 128, 128, 128);
-
     public void ShowWithTarget(StateMachine target) {
         machine = target;
         Show();
@@ -27,22 +25,36 @@ public class StateMachineEditor : EditorWindow {
         if (machine == null) {
             GUILayout.Label("No state machine selected!");
         } else {
+            var matrix = GUI.matrix;
+            //GUI.matrix = GUI.matrix * Matrix4x4.TRS(new Vector3(100, 100, 0), Quaternion.identity, Vector3.one);
             BeginWindows();
-                windowRect = GUILayout.Window(0, windowRect, DrawWindow, "Window", GUI.skin.box);
+            Vector2 stateSize = new Vector2(128, 64);
+            for (var i = 0; i < machine.states.Count; ++i) {
+                var rect = new Rect(machine.states[i].editorPosition-stateSize/2, stateSize);
+                rect = GUI.Window(i, rect, DrawWindow, new GUIContent(), GUI.skin.box);
+                machine.states[i].editorPosition = rect.position+stateSize/2;
+            }
             EndWindows();
+            GUI.matrix = matrix;
 
             GUILayout.BeginArea(new Rect(0, 0, 250, position.height), GUI.skin.box);
             GUILayout.Label(machine.gameObject.name);
             if (GUILayout.Button("Select Game Object")) {
                 Selection.activeGameObject = machine.gameObject;
             }
+            GUILayout.Space(32);
+            if (GUILayout.Button("Add State")) {
+                var state = new StateMachine.State();
+                state.name = machine.states.Count.ToString();
+                state.editorPosition = position.size / 2;
+                machine.states.Add(state);
+            }
             GUILayout.EndArea();
         }
     }
 
     void DrawWindow(int id) {
-        GUILayout.Button("Button");
-        GUILayout.Label("Content");
+        GUILayout.Label(machine.states[id].name);
         GUI.DragWindow();
     }
 }
--- Research Project/Assets/StateMachine/StateMachine.cs
+++ Research Project/Assets/StateMachine/StateMachine.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class StateMachine : MonoBehaviour {
 
+    [System.Serializable]
+    public class State {
+        public string name = "State";
+        public Vector2 editorPosition = Vector2.zero;
+    }
+
+    public List<State> states = new List<State>();
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
