202b0c66b2a66d12226d6c0b4041bca4e2b22327
Greg Weil <weilg@rpi.edu>
2016-03-05 17:18:02 -0500

    Reorganized editor code to make everything a bit cleaner

--- Research Project/Assets/Editor/StateMachineEditor.cs
+++ Research Project/Assets/Editor/StateMachineEditor.cs
@@ -10,7 +10,11 @@ public class StateMachineEditor : EditorWindow {
         editor.Show();
     }
 
+
     StateMachine machine = null;
+
+    Vector2 stateSize = new Vector2(128, 64);
+    float panelWidth = 250;
 
     public void ShowWithTarget(StateMachine target) {
         machine = target;
@@ -21,41 +25,52 @@ public class StateMachineEditor : EditorWindow {
         titleContent = new GUIContent("State Machine");
     }
 
+
     void OnGUI() {
         if (machine == null) {
             GUILayout.Label("No state machine selected!");
         } else {
-            var matrix = GUI.matrix;
-            //GUI.matrix = GUI.matrix * Matrix4x4.TRS(new Vector3(100, 100, 0), Quaternion.identity, Vector3.one);
-            BeginWindows();
-            Vector2 stateSize = new Vector2(128, 64);
-            for (var i = 0; i < machine.states.Count; ++i) {
-                var rect = new Rect(machine.states[i].editorPosition-stateSize/2, stateSize);
-                rect = GUI.Window(i, rect, DrawWindow, new GUIContent(), GUI.skin.box);
-                machine.states[i].editorPosition = rect.position+stateSize/2;
-            }
-            EndWindows();
-            GUI.matrix = matrix;
-
-            GUILayout.BeginArea(new Rect(0, 0, 250, position.height), GUI.skin.box);
-            GUILayout.Label(machine.gameObject.name);
-            if (GUILayout.Button("Select Game Object")) {
-                Selection.activeGameObject = machine.gameObject;
-            }
-            GUILayout.Space(32);
-            if (GUILayout.Button("Add State")) {
-                var state = new StateMachine.State();
-                state.name = machine.states.Count.ToString();
-                state.editorPosition = position.size / 2;
-                machine.states.Add(state);
-            }
+            DrawStateWindows();
+            GUILayout.BeginArea(new Rect(0, 0, panelWidth, position.height), GUI.skin.box);
+            DrawPanelContent();
             GUILayout.EndArea();
         }
     }
 
-    void DrawWindow(int id) {
+
+    void DrawStateWindow(int id) {
         GUILayout.Label(machine.states[id].name);
         GUI.DragWindow();
     }
+
+    void DrawStateWindows() {
+        BeginWindows();
+        Vector2 stateOffset = (position.size / 2) - (stateSize / 2);
+        stateOffset.x += panelWidth / 2;
+        for (var i = 0; i < machine.states.Count; ++i) {
+            var rect = new Rect(machine.states[i].editorPosition + stateOffset, stateSize);
+            rect = GUI.Window(i, rect, DrawStateWindow, new GUIContent(), GUI.skin.box);
+            machine.states[i].editorPosition = rect.position - stateOffset;
+        }
+        EndWindows();
+    }
+
+
+    void DrawPanelContent() {
+        GUILayout.Label(machine.gameObject.name);
+        if (GUILayout.Button("Select Game Object")) {
+            Selection.activeGameObject = machine.gameObject;
+        }
+
+        GUILayout.Space(16);
+
+        if (GUILayout.Button("Add State")) {
+            var state = new StateMachine.State();
+            state.name = machine.states.Count.ToString();
+            state.editorPosition = Vector2.zero;
+            machine.states.Add(state);
+        }
+    }
+
 }
 
