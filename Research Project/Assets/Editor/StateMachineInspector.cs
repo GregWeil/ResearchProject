4504b7385a336bbf754acfab08d1c77acb9a602d
Greg Weil <weilg@rpi.edu>
2016-03-29 23:45:55 -0400

    Added support for vectors

--- Research Project/Assets/Editor/StateMachineInspector.cs
+++ Research Project/Assets/Editor/StateMachineInspector.cs
@@ -6,8 +6,8 @@ using System.Collections;
 public class StateMachineInspector : Editor {
 
     int typeSelected = 0;
-    string[] typeNames = { "Bool", "Float", "Integer" };
-    System.Type[] typeTypes = { typeof(bool), typeof(float), typeof(int) };
+    string[] typeNames = { "Bool", "Float", "Integer", "Vector2", "Vector3" };
+    System.Type[] typeTypes = { typeof(bool), typeof(float), typeof(int), typeof(Vector2), typeof(Vector3) };
 
 	// Use this for initialization
 	void Start () {
@@ -34,15 +34,24 @@ public class StateMachineInspector : Editor {
         EditorGUILayout.LabelField("Parameters");
 
         EditorGUIUtility.labelWidth = 16;
+        float paramLabelWidth = (Screen.width - Mathf.Max(Screen.width * 0.4f, 160));
         foreach (var param in machine.parameters) {
             EditorGUILayout.BeginHorizontal();
-            param.name = EditorGUILayout.TextField(param.name, GUILayout.Width(Screen.width-128));
+            param.name = EditorGUILayout.TextField(param.name, GUILayout.Width(paramLabelWidth));
             if (param.type == typeof(bool)) {
                 param.value = EditorGUILayout.Toggle(" ", (bool)param.value);
             } else if (param.type == typeof(float)) {
                 param.value = EditorGUILayout.FloatField(" ", (float)param.value);
             } else if (param.type == typeof(int)) {
                 param.value = EditorGUILayout.IntField(" ", (int)param.value);
+            } else if (param.type == typeof(Vector2)) {
+                GUILayout.Space(EditorGUIUtility.labelWidth);
+                param.value = EditorGUILayout.Vector2Field(GUIContent.none, (Vector2)param.value);
+            } else if (param.type == typeof(Vector3)) {
+                GUILayout.Space(EditorGUIUtility.labelWidth);
+                param.value = EditorGUILayout.Vector3Field(GUIContent.none, (Vector3)param.value);
+            } else if (param.type == typeof(GameObject)) {
+                param.value = EditorGUILayout.ObjectField((GameObject)param.value, typeof(GameObject), true);
             }
             EditorGUILayout.EndHorizontal();
         }
