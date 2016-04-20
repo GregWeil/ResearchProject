68ee1ce1c3583cc75f3d3de3851aa891f65c1909
devin <dmanhilpert@yahoo.com>
2016-04-20 14:17:22 -0400

    fabulous prefabs

---
+++ Research Project/Assets/Environment/prefabs/pulsing.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pulsing : MonoBehaviour {
+    Material pulse;
+    public float rng = 1f;
+    public float spd = 1f;
+	// Use this for initialization
+	void Start () {
+        pulse = GetComponent<MeshRenderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pulse.SetColor("_EmissionColor", new Color(0f, 1f, 1f) *(rng*(Mathf.Sin((Time.time)*spd)+1)));
+	}
+}
