fe87f2126743f0a0e61e07858443dc93eb107a2d
Greg Weil <weilg@rpi.edu>
2016-04-27 19:47:13 -0400

    Added a script so the door can be locked and unlocked

---
+++ Research Project/Assets/Environment/Door.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    [SerializeField]
+    private bool locked = false;
+
+    HingeJoint hinge = null;
+    GameObject objLocked = null;
+    GameObject objUnlocked = null;
+
+	// Use this for initialization
+	void Start () {
+        hinge = GetComponentInChildren<HingeJoint>();
+        objLocked = transform.Find("Door").Find("Locked").gameObject;
+        objUnlocked = transform.Find("Door").Find("Unlocked").gameObject;
+        setDoor(locked);
+    }
+
+
+    public void lockDoor() {
+        setDoor(true);
+    }
+
+    public void unlockDoor() {
+        setDoor(false);
+    }
+
+    public void setDoor(bool newLocked) {
+        locked = newLocked;
+        hinge.useSpring = locked;
+        objLocked.SetActive(locked);
+        objUnlocked.SetActive(!locked);
+    }
+
+
+    void OnValidate() {
+        Start();
+    }
+}
