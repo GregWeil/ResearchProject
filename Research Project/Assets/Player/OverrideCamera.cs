0b393f06819229af6231cf1c491d32a514e0cf15
Greg Weil <weilg@rpi.edu>
2016-04-28 22:58:49 -0400

    Lock the player camera to a location while inside a trigger

---
+++ Research Project/Assets/Player/OverrideCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverrideCamera : MonoBehaviour {
+
+    Transform anchor = null;
+
+	// Use this for initialization
+	void Start () {
+        anchor = transform.Find("Anchor");
+	}
+	
+	void OnTriggerStay(Collider col) {
+        if (col.CompareTag("Player")) {
+            GameObject.FindObjectOfType<AnchoredCamera>().lockCamera(anchor.position, anchor.rotation, 0.25f);
+        }
+    }
+}
