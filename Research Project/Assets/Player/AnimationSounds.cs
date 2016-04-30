b4d14f282323684135e4285b883b88419b643809
Greg Weil <weilg@rpi.edu>
2016-04-30 05:59:40 -0400

    Added footstep sound

---
+++ Research Project/Assets/Player/AnimationSounds.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationSounds : MonoBehaviour {
+
+    public AudioClip footstep = null;
+
+    AudioSource source = null;
+
+    void Start () {
+        source = GetComponent<AudioSource>();
+    }
+
+	public void Footstep () {
+        source.PlayOneShot(footstep);
+    }
+}
