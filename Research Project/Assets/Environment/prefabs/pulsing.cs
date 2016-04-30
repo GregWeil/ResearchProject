9475e6f28e63e2c2ecbbd5e73389fc36a0d15c08
Greg Weil <weilg@rpi.edu>
2016-04-30 05:40:34 -0400

    Added a sound to the moving platforms

--- Research Project/Assets/Environment/prefabs/pulsing.cs
+++ Research Project/Assets/Environment/prefabs/pulsing.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class pulsing : MonoBehaviour {
     Material pulse;
+    AudioSource sound;
     public float rng = 1f;
     public float spd = 1f;
     public Color color = new Color(0f, 1f, 1f);
@@ -11,10 +12,13 @@ public class pulsing : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         pulse = GetComponent<MeshRenderer>().material;
+        sound = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        pulse.SetColor("_EmissionColor", color *(rng*(Mathf.Sin((Time.time)*spd)+origin)));
+        var intensity = (rng * (Mathf.Sin((Time.time) * spd) + origin));
+        pulse.SetColor("_EmissionColor", color * intensity);
+        if (sound != null) sound.volume = Mathf.Clamp01(intensity);
 	}
 }
